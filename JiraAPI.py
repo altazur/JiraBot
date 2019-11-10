896ede878a2e8645efcedd76372a3ef814bb7941
altazur <altazur.91@gmail.com>
2019-11-10 23:45:19 +0200

    Jira class folding original jira API added

---
+++ JiraAPI.py
@@ -0,0 +1,19 @@
+import os
+from jira import JIRA
+
+
+class JiraAPI(JIRA):
+    project = 'FOT'
+
+    def create_task(type='Bug', summary, description, assignee, priority):
+        project = self.project(project)
+        new_issue_dict = {
+                'project': {'id': project.id},
+                'summary': summary,
+                'description': description,
+                'issuetype': {'name': type},
+                'priority': priority,
+                'assignee': {'displayName', assignee}
+                }
+        return new_issue = self.create_issue(fields=new_issue_dict)
+
