4b0837596fd7abff50161da65018ad78655e07f5
altazur <altazur.91@gmail.com>
2019-11-10 20:55:30 +0200

    Base bot instantiate script is done

---
+++ bot.py
@@ -0,0 +1,51 @@
+import os
+import time
+import re
+from slackclient import SlackClient
+
+slack_client = SlackClient(os.environ('SLACK_BOT_TOKEN'))
+jirabot_id = None
+
+RTM_READ_DELAY = 1 #1 seconds to delay between reading
+EXAMPLE_COMMAND = "create bug Summary Description 6.5"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+if __name__ == "__main__":
+    """Instantiate bot and 'activatin it' by infinite Loop"""
+    if slack_client.rtm_connect(with_team_state=False):
+        print("JiraBot connected and running")
+        jirabot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection has been lost")
+
+def parse_bot_commands(slack_events):
+    """Return tuple (message, channel) if there is a bot command"""
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == jirabot_id:
+                return message, event["channel"]
+    return None, None
+
+def parse_direct_mention(message_text):
+    """If there is a mention returns user_id which was mentioned and the text after the mention. Otherwise return (None, None)"""
+    matches = re.search(MENTION_REGEX, message_text)
+    #The first group contains username, the second - the remaining message
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel):
+    """Executes command if the command is known"""
+    #Default response if the command is wrong"""
+    default_response = "I'm sorry, but the command isn't right. Try *{}*.".format(EXAMPLE_COMMAND)
+    response = None
+    if command.startswith("create bug"):
+        #response = ""
+        #TODO: Jira integration for commands
+        pass
+    #Send response to the channel
+    slack_client.api_call("chat.postMessage", channel=channel, text=response or default_response)
