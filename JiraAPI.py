9d62fda3280b78cff468502a2ae591569aa70de4
Anatoliy Romsa <anatoliy.romsa@qa-testlab.com>
2020-04-23 18:13:34 +0300

    Remove unnecessary default arg value

--- JiraAPI.py
+++ JiraAPI.py
@@ -4,7 +4,7 @@ from jira import JIRA
 
 class JiraAPI(JIRA):
 
-    def create_task(self, issue_type, summary, description, assignee, priority, project_name='FOT'):
+    def create_task(self, issue_type, summary, description, assignee, priority, project_name):
         project = self.project(project_name)#Calling parent JIRA.project func and passing the our project name into it
         new_issue_dict = {
                 'project': {'id': project.id},
