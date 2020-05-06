75209b2c01d5c1999c747f7c025a77d3cbefc7df
altazur <altazur.91@gmail.com>
2020-05-06 22:48:29 +0300

    Add additional tests

--- test_bot_command_parser.py
+++ test_bot_command_parser.py
@@ -4,7 +4,10 @@ from bot_command_parser import get_summary_from_message, get_project_from_messag
 @pytest.mark.parametrize("test_message, test_cmd_list, expected", 
         [
             ("create_bug Fix some value within the field", ["create_bug", "Assignee:"], "Fix some value within the field"), 
-            ("create_improvement Summary of the issue Project: Assignee:", ["create_improvement", "Project:", "Assignee:"], "Summary of the issue")
+            ("create_improvement Summary of the issue Project: Assignee:", ["create_improvement", "Project:", "Assignee:"], "Summary of the issue"),
+            ("create_task Some task Assignee: some Project: PRO", ["create_task", "Project:", "Assignee:"], "Some task"),
+            ("create_task task Project:  Assignee:  ", ["create_task", "Project:", "Assignee:"], "task"),
+            ("create_improvement Some serious test impr Assignee: test.test Project: TST", ["create_improvement", "Project:", "Assignee:"], "Some serious test impr")
             ])
 def test_get_summary_from_message(test_message, test_cmd_list, expected):
     assert get_summary_from_message(test_cmd_list, test_message) == expected
@@ -18,7 +21,8 @@ def test_error_get_summary_from_message():
 @pytest.mark.parametrize("test_msg, test_cmd, expected", 
         [
             ("create_story Some story Project: BRU Assignee: anatoliy.anatoliy", "Project:", "BRU"), 
-            ("create_task Some stru without project Assignee: test.test", "Project:", "FOT")
+            ("create_task Some task without project Assignee: test.test", "Project:", "FOT"),
+            ("create_task Some task without project Assignee: ", "Project:", "FOT")
             ])
 def test_get_project_from_message(test_msg, test_cmd, expected):
     assert get_project_from_message(test_msg, test_cmd) == expected
@@ -27,7 +31,8 @@ def test_get_project_from_message(test_msg, test_cmd, expected):
         [
             ("create_improvement Some impr of the issue Assignee: test.test", "Assignee:", "test.test"), 
             ("create_bug Some bug Project: FOT Assignee: me", "Assignee:", "me"), 
-            ("create_bug Find some beer", "Assignee:", "Anatoliy.Romsa")
+            ("create_bug Find some beer", "Assignee:", "Anatoliy.Romsa"),
+            ("create_bug bug Assignee:  Project:", "Assignee:", "Anatoliy.Romsa")
             ])
 def test_get_assignee_from_message(test_msg, test_cmd, expected):
     assert get_assignee_from_message(test_msg, test_cmd) == expected
